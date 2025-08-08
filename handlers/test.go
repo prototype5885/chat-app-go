10f94273ba439836d78e65544eea2054aed33743
prototype585 <prototype585@protonmail.com>
2025-08-08 19:20:40 +0200

    simplified /api/test

--- handlers/test.go
+++ handlers/test.go
@@ -1,30 +1,7 @@
 package handlers
 
-import (
-	"chatapp-backend/utils/snowflake"
-	"fmt"
-	"net/http"
-
-	"github.com/vmihailenco/msgpack/v5"
-)
+import "net/http"
 
 func Test(w http.ResponseWriter, r *http.Request) {
-	type Test struct {
-		First  []uint64 `msgpack:"first"`
-		Second []string `msgpack:"second"`
-	}
-
-	test := Test{}
-
-	for range 10000 {
-		id, _ := snowflake.Generate()
-		test.First = append(test.First, id)
-		test.Second = append(test.Second, fmt.Sprint(id))
-	}
-
-	err := msgpack.NewEncoder(w).Encode(test)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	w.Write([]byte("Hello World"))
 }
